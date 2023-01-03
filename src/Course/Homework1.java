e365ebd9c38f45cec2dc346741eef1581be135ed
DESKTOP-PNRUT96\Dzhamilia <dzhamilya7770@gmail.com>
2023-01-03 22:36:31 +0200

    Lesson1

---
+++ src/Course/Homework1.java
@@ -0,0 +1,30 @@
+package Course;
+
+public class Homework1 {
+
+    public static void main(String[] args){
+
+        String name = "Dzhamilia";
+        String surname = "Zikirova";
+        String space = " ";
+        String info = name + space + surname;
+        System.out.println(info);
+
+
+        double a = 5/10D;
+        double b = 20/5D;
+        double res1 = a + b;
+        System.out.println(res1);
+
+
+        int number = 789;
+        int hundredsAmount = number/100;
+        System.out.println(hundredsAmount);
+        int tensAmount = number/10;
+        System.out.println(tensAmount);
+
+        int unitAmount = number/1;
+        System.out.println(unitAmount);
+    }
+}
+
